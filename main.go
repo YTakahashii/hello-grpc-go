fbebb2e204bf2005d1941519580f814db7530f4a
髙橋 佑太 <yuta.takahashi@jbs.com>
2019-03-09 16:49:22 +0900

    :sparkles: 最も簡単な実装
    # ==== Emojis ====
    # 🎉  :tada: Initial commit
    # 🔖  :bookmark: バージョンタグ
    # 🐛  :bug: バグ修正
    # ✏️  :pencil2: タイポ修正
    # 👍  :+1: 機能改善
    # ✨  :sparkles: 部分的な機能追加
    # 💄  :lipstick: UIやスタイルファイルのアップデート
    # 📝  :memo: ドキュメントやコメントの追加
    # ♻️  :recycle: リファクタリング
    # 🗑️  :wastebasket: 不要な機能・使われなくなった機能の削除
    # ✅  :white_check_mark: テスト実装
    # 💚  :green_heart: テストやCIの修正・改善
    # 👕  :shirt: Lintエラーの修正やコードスタイルの修正
    # 🚀  :rocket: パフォーマンス改善
    # ➕  :heavy_plus_sign: 依存パッケージの追加
    # ➖  :heavy_minus_sign: 依存パッケージの削除
    # 🆙  :up: 依存パッケージのアップデート
    # 🔒  :lock: 新機能の公開範囲の制限
    # 👮  :cop: セキュリティ関連の改善
    # 🚧  :construction: WIP(Work In Progress)
    # 🙈  :see_no_evil: .gitignoreファイルの更新
    # 🏷️  :label: 型の追加やアップデート
    # 🐳  :whale: Dockerの作業

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 	"net"
+
 	rpc_ping "github.com/YTakahashii/hello-grpc-go/interfaces/rpc/ping"
 	"google.golang.org/grpc"
 )
@@ -12,23 +13,9 @@ type server struct{}
 
 const port = ":3001"
 
-// implements
-type IPingImplement interface {
-	Ping(ctx context.Context, p *rpc_ping.Empty) (*rpc_ping.Pong, error)
-}
-
-type pingImplement struct {
-}
-
-// NewPingImplement is ...
-func NewPingImplement() IPingImplement {
-	return &pingImplement{}
-}
-
-func (imp *pingImplement) Ping(ctx context.Context, p *rpc_ping.Empty) (*rpc_ping.Pong, error) {
-	res := rpc_ping.Pong{}
-	res.Reply = "Pong"
-	return &res, nil
+// SayHello implements helloworld.GreeterServer
+func (s *server) Ping(ctx context.Context, in *rpc_ping.Empty) (*rpc_ping.Pong, error) {
+	return &rpc_ping.Pong{Reply: "Hello"}, nil
 }
 
 func main() {
@@ -36,6 +23,7 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
-	var grpcServer = grpc.NewServer()
+	grpcServer := grpc.NewServer()
+	rpc_ping.RegisterCheckServer(grpcServer, &server{})
 	grpcServer.Serve(listenPort)
 }
